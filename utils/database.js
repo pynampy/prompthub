03b168c927e9b141daf7314577fcb5b5fa81488a
Naman Jain <naman@Namans-Macbook.local>
2024-08-21 01:51:03 +0530

    Setting up basic structure of project and adding the homepage

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDb = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log("MongoDB connected!");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "PromptHub",
+            userNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        isConnected = true;
+
+        console.log("MongoDB connected!")
+    } catch (error) {
+        console.log(error)
+    }
+}
