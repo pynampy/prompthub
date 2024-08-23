06ea34ce4d88e2fd984afe4e9bd9a69ae134cecb
Naman Jain <naman@Namans-Macbook.local>
2024-08-23 08:27:56 +0530

    Profile Page

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,6 +1,26 @@
 import { connectToDb } from "@utils/database";
 import Prompt from '@models/prompt';
 
+export const GET = async (req, res) => {
+    try {
+        await connectToDb();
+        const newPrompt = new Prompt({
+            creator: userId,
+            prompt,
+            tag
+        })
+
+        await newPrompt.save();
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
+
+    } catch (e) {
+        console.log(error);
+        return new Response("Failed to create a new prompt", { status: 201 })
+
+    }
+}
+
+
 export const POST = async (req, res) => {
     const { userId, prompt, tag } = await req.json();
 
